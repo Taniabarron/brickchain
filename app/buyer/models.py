42a62e7bac44fb2baff1170fc1d49b8bb1dff8bb
Tania Barron Rosas <taniabarronrosas@MacBook-Pro-de-Tania.local>
2024-11-16 00:45:19 +0700

    feat/redirect urls, settings change, include datos.json file in utils

--- app/buyer/models.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from app.core.views import *
-
-app_name = 'app.core'
-
-urlpatterns = [
-]
--- config/urls.py
+++ config/urls.py
@@ -14,9 +14,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    #core
+    path('', include('app.core.urls')), 
+    #core
+    path('', include('app.marketplace.urls')), 
+    
+    path('', include('app.seller.urls')), 
+    
+    path('', include('app.buyer.urls')), 
+    
 ]
